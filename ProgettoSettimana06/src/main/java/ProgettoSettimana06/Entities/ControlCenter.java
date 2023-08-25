72f5a46f7ca3d91d12cb19777a0c2474faee6640
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-08-25 12:22:04 +0200

    Implementata classe ControlCenter con Costruttore e variabili

--- ProgettoSettimana06/src/main/java/ProgettoSettimana06/Entities/ControlCenter.java
+++ ProgettoSettimana06/src/main/java/ProgettoSettimana06/Entities/ControlCenter.java
@@ -1,5 +1,38 @@
 package ProgettoSettimana06.Entities;
 
-public class ControlCenter {
+import java.util.ArrayList;
+import java.util.List;
+
+import ProgettoSettimana06.Interface.Observer;
+import ProgettoSettimana06.Interface.Subject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ControlCenter implements Subject {
+
+	private Long id;
+	private List<Sensor> sensors = new ArrayList<>();
+
+	@Override
+	public void register(Observer observer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unregister(Observer observer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyObserversChanges() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
