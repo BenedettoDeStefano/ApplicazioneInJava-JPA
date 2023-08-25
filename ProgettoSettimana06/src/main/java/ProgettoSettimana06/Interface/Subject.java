fba7d58b3776e6183524c303f718792f0c612a6e
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-08-25 11:47:06 +0200

    Creazione Struttura Interface

---
+++ ProgettoSettimana06/src/main/java/ProgettoSettimana06/Interface/Subject.java
@@ -0,0 +1,5 @@
+package ProgettoSettimana06.Interface;
+
+public interface Subject {
+
+}
---
+++ ProgettoSettimana06/src/test/java/ProgettoSettimana06/ProgettoSettimana06ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProgettoSettimana06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
