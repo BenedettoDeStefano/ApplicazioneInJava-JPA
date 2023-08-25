02e589a1e3cf03bcf3e6dbf2526531de0590be80
Benedetto <129066047+BenedettoDeStefano@users.noreply.github.com>
2023-08-25 12:13:01 +0200

    Implementata classe Sensor con variabili e costruttore

--- ProgettoSettimana06/src/main/java/ProgettoSettimana06/Entities/Sensor.java
+++ ProgettoSettimana06/src/main/java/ProgettoSettimana06/Entities/Sensor.java
@@ -1,8 +1,17 @@
 package ProgettoSettimana06.Entities;
 
 import ProgettoSettimana06.Interface.Observer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@AllArgsConstructor
+@Data
 public class Sensor implements Observer {
+
+	private Long id;
+	private Integer smokeLevel = 0;
+	private String latitude;
+	private String longitude;
 
 	@Override
 	public void checkSmokeLevel() {
